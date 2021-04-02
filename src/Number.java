2a17df2ba01ca636e1d32225fe0842ff80ab89e0
akkadam <kadam.akash44@gmail.com>
2021-04-02 23:48:37 +0530

    added by ak

--- src/Number.java
+++ src/Number.java
@@ -1,6 +1,10 @@
 
 public class Number {
 
+	public void test1() {
+		System.out.println("test1");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("aaaa");
